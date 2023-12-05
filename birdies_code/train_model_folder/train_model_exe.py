343dff7bda8930e80ea5267736e64faccd3cae40
Bruno Thormählen <brunothormaehlen@yahoo.com>
2023-12-05 17:47:52 +0100

    train script ready

--- birdies_code/train_model_folder/train_model_exe.py
+++ birdies_code/train_model_folder/train_model_exe.py
@@ -1,3 +1,3 @@
 from birdies_code.train_model_folder.train_model import run_all
 
-run_all(data_dir="birdies_code/train_model_folder/image_data", model_dir="birdies_code/train_model_folder/saved_model")
+run_all(data_dir="birdies_code/train_model_folder/image_data", save_dir="birdies_code/train_model_folder/saved_model")
